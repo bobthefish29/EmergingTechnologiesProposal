6b45a02f5554bf28080c63b430a850f8d75be876
bobthefish29 <dpunchak@icloud.com>
2026-09-24 18:00:27 -0400

    Demo

--- tests/app.test.js
+++ tests/app.test.js
@@ -10,8 +10,8 @@ describe("Chatbot application", () => {
     test("The application responds", async () => {
         const response = await request(app).get("/");
         //Uncomment this line to show what a faile would do in github actions and how the changes were not made live
-        expect(response.status).toBe(500);
-        // expect(response.status).toBe(200);
+        // expect(response.status).toBe(500);
+        expect(response.status).toBe(200);
     });
 
 });
